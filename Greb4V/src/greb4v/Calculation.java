1c9fa03d0106a4ab1b90fac90fcba1a57a266fe5
Muhammad Iqhmal Bin Azman (U2001667) <u2001667@siswa.um.edu.my>
2022-06-01 22:17:54 +0800

    Merge origin/master
    Conflicts:
    	Greb4V/src/greb4v/Main.java

--- Greb4V/src/greb4v/Calculation.java
+++ Greb4V/src/greb4v/Calculation.java
@@ -1,18 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package greb4v;
 
-/**
- *
- * @author User
- */
 public class Calculation {
 
     private int min;
     private int hr = 0;
 
+    // assuming speed 60km/h
     public void Calculation(double distance) {
         int min;
         int hr = 0;
--- Greb4V/src/greb4v/Customer.java
+++ Greb4V/src/greb4v/Customer.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package greb4v;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author User
- */
 public class Customer {
     ArrayList<CustomerProfile> customer = new ArrayList<CustomerProfile>();
 
     public void add(CustomerProfile e) {
         customer.add(e);
+        
     }
 
     public void remove(String name) {
@@ -34,14 +27,6 @@ public class Customer {
 
     public void display() {
         System.out.println("=====================================================================================================================");
-        System.out.printf("%-20s %-20s %-20s %-20s %-20s %-20s\n", "Customer", "Status", "Expected Arrival Time",
-                "Capacity", "Starting Point", "Destination");
-        for (CustomerProfile customerProfile : customer) {
-            System.out.printf("%-20s %-20s %-20s %-20s %-20s, %-20s\n", customerProfile.getName(), "Status", 
-                customerProfile.getEAT(), customerProfile.getCap(), customerProfile.getIniLan() + "," + 
-                    customerProfile.getIniLong(), customerProfile.getFiLan() + "," + customerProfile.getFiLong());
-        }
-
-        System.out.println("=====================================================================================================================");
+        System.out.printf("%-20s %-20s q=======================================================================================");
     }
 }
