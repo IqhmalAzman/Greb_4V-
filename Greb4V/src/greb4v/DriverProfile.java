4bdcf9114fd96dd84a5821151e165e6ca3a7cd97
erinkxw <92709859+erinkxw@users.noreply.github.com>
2022-06-01 21:36:32 +0800

    Update DriverProfile.java

--- Greb4V/src/greb4v/DriverProfile.java
+++ Greb4V/src/greb4v/DriverProfile.java
@@ -11,10 +11,29 @@ package greb4v;
 public class DriverProfile extends Profile{
     
     private String status, customer;
+    private double rating;
+    private int ratingCount;
     
     public DriverProfile(String name, int cap, double iniLan, double iniLong) {
         super(name, cap, iniLan, iniLong);
         status = "available";
+        rating = 5.00;
+        ratingCount++;
     }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating += rating;
+        this.rating = this.rating/ratingCount;
+        this.rating = round(this.rating, 1);
+    }
+    
+    private static double round (double value, int precision) {
+    int scale = (int) Math.pow(10, precision);
+    return (double) Math.round(value * scale) / scale;
+}
     
 }
