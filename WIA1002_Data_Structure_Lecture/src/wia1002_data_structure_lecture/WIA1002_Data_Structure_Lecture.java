c5c8ec130c879b0e4cea661defec0563f37d531c
Muhammad Iqhmal Bin Azman (U2001667) <u2001667@siswa.um.edu.my>
2022-06-09 07:11:55 +0800

    Merge origin/master
    Conflicts:
    	Greb4V/src/greb4v/Main.java

---
+++ WIA1002_Data_Structure_Lecture/src/wia1002_data_structure_lecture/WIA1002_Data_Structure_Lecture.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wia1002_data_structure_lecture;
+
+/**
+ *
+ * @author Admin
+ */
+public class WIA1002_Data_Structure_Lecture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
