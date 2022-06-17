e45391ba71b39a74da8602230cfd27c419a8591d
erinkxw <92709859+erinkxw@users.noreply.github.com>
2022-06-17 20:30:16 +0800

    Update Time.java

--- Greb4V/src/Utils/Time.java
+++ Greb4V/src/Utils/Time.java
@@ -3,15 +3,19 @@ package Utils;
 import java.util.Date;
 
 public class Time {
-    long startTime;
-    long stopTime;
+    private final long startTime;
+    private long stopTime;
 
+    // this is where we initialized and get the initial value of time from the computer
     public Time() {
         Date d = new Date();
         
         startTime = d.getTime();
     }
 
+    // this is where we get the recentest time so that we can
+    // calculate the difference form the initial set time/startTime
+    // in the constructor
     public String time() {
         Date d = new Date();
         
@@ -21,25 +25,23 @@ public class Time {
     }
 
     public String calcTime(long t1, long t2) {
-        
         int time = (int) (t2 - t1);
-        int timeInSec = (time / 1000);
+        int timeInSec = (time / 1000); //this is an actual time in second, however it could be interpreted as minute as we assume min = sec and hour = min
 
-        int mm = (timeInSec / 60) % 24; //24 is because we don't want to get over
-        int ss = (timeInSec % 60);
+        int hh = (timeInSec / 60) % 24; //24 is because we don't want to get it over; So it'll start from 00 again 
+        int mm = (timeInSec % 60);      
 
+        String displayHH = String.valueOf(hh);
         String displayMM = String.valueOf(mm);
-        String displaySS = String.valueOf(ss);
+
+        if (hh < 10) {
+            displayHH = "0" + displayHH;
+        }
 
         if (mm < 10) {
             displayMM = "0" + displayMM;
         }
 
-        if (ss < 10) {
-            displaySS = "0" + displaySS;
-        }
-
-        return displayMM + displaySS;
+        return displayHH + displayMM;
     }
-
 }
